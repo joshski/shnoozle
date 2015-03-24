827e5c02e9c1d5b45d737064864edb334c1ccd16
leomdivani <leomdiv@gmail.com>
2015-03-24 12:29:05 +0000

    record,pause,stop play audio

--- Shnoozle/MainViewController.h
+++ Shnoozle/MainViewController.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface MainViewController : UIViewController
+@interface MainViewController : UIViewController <AVAudioRecorderDelegate, AVAudioPlayerDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *logOutButton;
+@property (weak, nonatomic) IBOutlet UIButton *recordPauseBtn;
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+@property (weak, nonatomic) IBOutlet UIButton *stopBtn;
 
 @end
--- Shnoozle/MainViewController.m
+++ Shnoozle/MainViewController.m
@@ -6,27 +6,118 @@
 #import <ParseFacebookUtils/PFFacebookUtils.h>
 #import <FacebookSDK/FacebookSDK.h>
 
-@interface MainViewController ()
+@interface MainViewController (){
+    AVAudioRecorder *recorder;
+    AVAudioPlayer *player;
+}
 
 @end
 
 @implementation MainViewController
 
-- (void)viewDidLoad {
+@synthesize playBtn;
+@synthesize stopBtn;
+@synthesize recordPauseBtn;
+
+
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // Disable Stop/Play button when application launches
+    [stopBtn setEnabled:NO];
+    [playBtn setEnabled:NO];
+    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyAudioMemo.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+    // Define the recorder setting
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:NULL];
+    recorder.delegate = self;
+    recorder.meteringEnabled = YES;
+    [recorder prepareToRecord];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+- (IBAction)playBtnClicked:(id)sender {
+    if (!recorder.recording){
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+    }
+}
+- (IBAction)recordBtnClicked:(id)sender {
+    // Stop the audio player before recording
+    if (player.playing) {
+        [player stop];
+    }
+    
+    if (!recorder.recording) {
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setActive:YES error:nil];
+        
+        // Start recording
+        [recorder record];
+        [recordPauseBtn setTitle:@"Pause" forState:UIControlStateNormal];
+        
+    } else {
+        
+        // Pause recording
+        [recorder pause];
+        [recordPauseBtn setTitle:@"Record" forState:UIControlStateNormal];
+    }
+    
+    [stopBtn setEnabled:YES];
+    [playBtn setEnabled:NO];
+}
+
+- (IBAction)pauseBtnClicked:(id)sender {
+    [recorder stop];
+    
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setActive:NO error:nil];
+}
+
+
 - (IBAction)logOutClicked:(id)sender {
     [PFUser logOut];
     [self performSegueWithIdentifier:@"logOutClicked" sender:self];
 
 
 }
-
+- (void) audioRecorderDidFinishRecording:(AVAudioRecorder *)avrecorder successfully:(BOOL)flag{
+    [recordPauseBtn setTitle:@"Record" forState:UIControlStateNormal];
+    
+    [stopBtn setEnabled:NO];
+    [playBtn setEnabled:YES];
+}
+- (void) audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag{
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Done"
+                                                    message: @"Finish playing the recording!"
+                                                   delegate: nil
+                                          cancelButtonTitle:@"OK"
+                                          otherButtonTitles:nil];
+    [alert show];
+}
 
 @end

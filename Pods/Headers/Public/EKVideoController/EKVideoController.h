b72482b71f6aacf88aca53451eab6ba1b23fb2ad
Josh Chisholm <joshuachisholm@gmail.com>
2015-04-03 11:36:53 +0100

    remove pods

--- Pods/Headers/Public/EKVideoController/EKVideoController.h
+++
@@ -1,1 +0,0 @@
-../../../EKVideoController/EKVideoController/EKVideoController.h
--- Pods/Target Support Files/Pods-ShnoozleTests/Pods-ShnoozleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ShnoozleTests : NSObject
-@end
-@implementation PodsDummy_Pods_ShnoozleTests
-@end
